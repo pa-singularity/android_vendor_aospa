4d52b2ec9371eb9e26efc37c7970294b60ffe27f
Gustavo Mendes <gusttavo.me@outlook.com>
2024-01-26 12:57:17 -0300

    aospa: Ship Nexus launcher & Pixel theme picker
    Change-Id: Ia1806420119b2fdc49c35d81b683d53cdf4c341c
    Signed-off-by: Gustavo Mendes <gusttavo.me@outlook.com>

--- target/product/aospa-target.mk
+++ target/product/aospa-target.mk
@@ -60,7 +60,7 @@ PRODUCT_SYSTEM_EXT_PROPERTIES += \
 DONT_DEXPREOPT_PREBUILTS := true
 
 PRODUCT_DEXPREOPT_SPEED_APPS += \
-    Launcher3QuickStep \
+    NexusLauncherRelease \
     ParanoidSystemUI
 
 PRODUCT_PROPERTY_OVERRIDES += \
